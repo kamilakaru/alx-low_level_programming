31f074226849e7bda29b30683366c566370d37a0
root <karukamila@gmail.com>
2022-03-12 10:28:27 -0800

    low programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Always o (success)
+ */
+char* my_str="programming is like building a multilingual puzzle\n";
+int main(void)
+{
+	puts(my_str);
+	return(0);
+}
+
