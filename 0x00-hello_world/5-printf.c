760ecdde93468eaa18dfa2e086bb126ea7415a99
root <karukamila@gmail.com>
2022-03-12 12:13:07 -0800

    low programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * main - Print on the screen
- *
- * Return: zero
- */
+/**
+  * main - Prints onto screen
+  *
+  * Return: zero
+  */
 int main(void)
 {
-	printf("With proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
