0541faa3d8ce7195f694b8f677995bd9b5526c42
root <karukamila@gmail.com>
2022-03-12 10:49:04 -0800

    low programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-/*
- * main - Entry point
- *
- * Return: Always o (success)
- */
-char* my_str="programming is like building a multilingual puzzle\n";
+/**
+  * main - print to screen
+  *
+  * Return: zero
+  */
 int main(void)
 {
-	puts(my_str);
+	char mystr[] = "\"programming is like building a multilingual puzzle";
+
+	puts(mystr);
 	return(0);
 }
 
