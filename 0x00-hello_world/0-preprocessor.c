2851880b5d46930c4300310a6ea2b336937f4c01
kamilakaru <karukamila@gmail.com>
2022-03-12 08:10:20 -0800

    preprocessor

---
+++ 0x00-hello_world/0-preprocessor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+char* my_str=
+"#!/bin/bash\n"
+"gcc -E $CFILE >> c";
+int main(void)
+{
+	printf("%s\n", my_str);
+	return(0);
+}
+
