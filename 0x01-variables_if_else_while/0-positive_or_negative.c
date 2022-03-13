69304b01f78511bc3b4d770fc53e9fea5acabd4c
kamilakaru <karukamila@gmail.com>
2022-03-13 09:04:47 -0700

    variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - determine if number is positive or negative
  *
- * Description: the program will assign a random number to the variable n each time
+ * the program will assign a random number to the variable n each time
  * it is executed
  *
  * Return: zero
@@ -21,7 +21,7 @@ int main(void)
 		printf("%d is zero\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
-	else 
+	else
 		printf("%d is positive\n", n);
 	return (0);
 }
