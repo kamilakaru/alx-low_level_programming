2bec9e1478d2648806ae44be8713b87634f38eb3
kamilakaru <karukamila@gmail.com>
2022-03-13 03:39:12 -0700

    low programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/**
-  * main - Prints to the standard error
-  *
-  * Return: one
-  */
+/*
+ * main - Prints to the standard error
+ * 
+ * Return: one
+ */
 int main(void)
 {
 	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
