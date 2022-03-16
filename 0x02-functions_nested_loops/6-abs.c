023647f775b4602a5f9ab39428fa24a844dc55ed
kamilakaru <karukamila@gmail.com>
2022-03-16 16:37:27 +0000

    low programming

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: input
+ *
+ * Return: the absolute value of the inter
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
