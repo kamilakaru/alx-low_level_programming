aa427b139f84f8e7f3f028a90831b6fe62372008
kamilakaru <karukamila@gmail.com>
2022-03-16 18:49:55 +0000

    low programming

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - two integers
+ * @num1: input
+ * @num2: input
+ *
+ * Return: the result of the addition
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
