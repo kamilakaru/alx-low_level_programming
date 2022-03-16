a2cc0fee10570c2ec790760fb827afab270022ea
kamilakaru <karukamila@gmail.com>
2022-03-16 15:27:26 +0000

    low programming

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - in lowercase
+ *
+ * Description: print alphabet x10 in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
