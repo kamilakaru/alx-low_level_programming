5315933d48ea9af8e86717d12f4b0dc5e2480709
kamilakaru <karukamila@gmail.com>
2022-03-17 16:57:01 +0000

    loe programming

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -7,45 +7,46 @@
 * @day: day of month
 * Return: day of year
 */
+
 int convert_day(int month, int day)
 {
 	switch (month)
 	{
 		case 2:
 			day = 31 + day;
-		break;
+			break;
 		case 3:
 			day = 59 + day;
-		break;
+			break;
 		case 4:
 			day = 90 + day;
-		break;
+			break;
 		case 5:
 			day = 120 + day;
-		break;
+			break;
 		case 6:
 			day = 151 + day;
-		break;
+			break;
 		case 7:
 			day = 181 + day;
-		break;
+			break;
 		case 8:
 			day = 212 + day;
-		break;
+			break;
 		case 9:
 			day = 243 + day;
-		break;
+			break;
 		case 10:
 			day = 273 + day;
-		break;
+			break;
 		case 11:
 			day = 304 + day;
-		break;
+			break;
 		case 12:
 			day = 334 + day;
-		break;
+			break;
 		default:
-		break;
+			break;
 	}
 	return (day);
 }
