726d7d8f6dad7aae6540553e96c811b65b0378ff
kamilakaru <karukamila@gmail.com>
2022-03-17 20:04:57 +0000

    low programming

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print string 0-9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; 1 < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
