6382455865c28aaf1d6fd43bc0b2684840b9add5
kamilakaru <karukamila@gmail.com>
2022-03-18 19:39:16 +0000

    low programming

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,20 @@
 #include "main.h"
-
 /**
- * print_most_numbers - print most except 2 and 4
+ *print_most_numbers - prints most except 2 and 4.
  *
- * Return: void
+ *Return: void.
  */
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <  10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		if ((i == 2) || (i == 4))
 		{
 			continue;
 		}
-		_putchar('0' + 1);
+		_putchar('0' + i);
 	}
 	_putchar('\n');
 }
