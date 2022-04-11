f53b3827d5ca90a5bcfbab5ec362d1766140ffc8
kamilakaru <karukamila@gmail.com>
2022-04-11 20:40:15 +0000

    low programming

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - prints name of the file
+ * Description: prints name of file
+ * return: Alaways (0)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
